386818fae0d2272132004ccc6c0f5391f014129c
rashidya <rashmiliyanage2000@gmail.com>
2022-08-16 10:21:24 +0530

    updated

--- src/controller/ChattingFormController.java
+++ src/controller/ChattingFormController.java
@@ -2,7 +2,22 @@ package controller;
 
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
+
 
 import java.io.*;
 import java.net.Socket;
@@ -10,6 +25,8 @@ import java.net.Socket;
 public class ChattingFormController {
     public JFXTextField txtMessage;
     public JFXTextArea messageDescription;
+    public ScrollPane scrollPane;
+    public VBox vBOxMessage;
 
     private Socket socket;
     private BufferedReader bufferedReader;
@@ -27,6 +44,13 @@ public class ChattingFormController {
                 this.bufferedWriter.newLine();
                 this.bufferedWriter.flush();
 
+                vBOxMessage.heightProperty().addListener(new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                        scrollPane.setVvalue((Double) newValue);
+                    }
+                });
+
 
                 //-------------------Update all Messages------------------------
                 new Thread(()-> {
@@ -34,8 +58,83 @@ public class ChattingFormController {
                     while (socket.isConnected()){
                         try{
                             chatMessages = bufferedReader.readLine();
-                            System.out.println(chatMessages);
-                            messageDescription.appendText(chatMessages+"\n");
+                            if (chatMessages.startsWith("img")){
+
+                                String[] split = chatMessages.replace("img", " ").split("-");
+
+
+                                HBox hBox = new HBox();
+                                hBox.setAlignment(Pos.CENTER_LEFT);
+                                hBox.setPadding(new Insets(5, 5, 5, 10));
+
+                                Text text = new Text(split[0] +" : ");
+                                TextFlow textFlow = new TextFlow(text);
+                                textFlow.setStyle("-fx-font-weight: bold;"+"-fx-background-color:#cf8bf6;");
+                                textFlow.setPadding(new Insets(5, 10, 5, 10));
+                                text.setFill(Color.color(1, 1, 1, 1));
+
+
+                                ImageView imageView = new ImageView();
+                                //Setting image to the image view
+                                imageView.setImage(new Image(new File(split[1]).toURI().toString()));
+                                //Setting the image view parameters
+                                imageView.setFitWidth(300);
+                                imageView.setPreserveRatio(true);
+
+                                hBox.getChildren().add(textFlow);
+                                hBox.getChildren().add(imageView);
+
+                                Platform.runLater(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        vBOxMessage.getChildren().add(hBox);
+                                    }
+                                });
+
+                            }else if (chatMessages.startsWith("SERVER")){
+
+                                HBox hBox = new HBox();
+                                hBox.setAlignment(Pos.CENTER);
+                                hBox.setPadding(new Insets(5, 5, 5, 10));
+
+                                Text text = new Text(chatMessages);
+                                TextFlow textFlow = new TextFlow(text);
+
+                                textFlow.setStyle("-fx-font-weight: bold;"+"-fx-background-color:#cf8bf6;" + "-fx-background-radius:10px");
+                                textFlow.setPadding(new Insets(5, 10, 5, 10));
+                                text.setFill(Color.color(1, 1, 1, 1));
+
+                                hBox.getChildren().add(textFlow);
+
+                                Platform.runLater(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        vBOxMessage.getChildren().add(hBox);
+                                    }
+                                });
+
+                            }else {
+                                HBox hBox = new HBox();
+                                hBox.setAlignment(Pos.CENTER_LEFT);
+                                hBox.setPadding(new Insets(5, 5, 5, 10));
+
+                                Text text = new Text(chatMessages);
+                                TextFlow textFlow = new TextFlow(text);
+
+                                textFlow.setStyle("-fx-font-weight: bold;"+"-fx-background-color:#cf8bf6;" + "-fx-background-radius:10px");
+                                textFlow.setPadding(new Insets(5, 10, 5, 10));
+                                text.setFill(Color.color(1, 1, 1, 1));
+
+                                hBox.getChildren().add(textFlow);
+
+                                Platform.runLater(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        vBOxMessage.getChildren().add(hBox);
+                                    }
+                                });
+                            }
+
                         }catch (IOException e){
                             closeEverything(socket,bufferedReader,bufferedWriter);
                         }
@@ -52,7 +151,21 @@ public class ChattingFormController {
 
     public void sendMessageOnAction(ActionEvent actionEvent) throws IOException {
         String message = txtMessage.getText();
-        messageDescription.appendText("Me : " + message+"\n");
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+        hBox.setPadding(new Insets(5, 5, 5, 10));
+
+        Text text = new Text("Me : " + message);
+        TextFlow textFlow = new TextFlow(text);
+
+        textFlow.setStyle("-fx-font-weight: bold;"+"-fx-background-color:#cf8bf6;" + "-fx-background-radius:10px");
+        textFlow.setPadding(new Insets(5, 10, 5, 10));
+        text.setFill(Color.color(1, 1, 1, 1));
+
+        hBox.getChildren().add(textFlow);
+        vBOxMessage.getChildren().add(hBox);
+
         bufferedWriter.write(userName + " : " + message);
         bufferedWriter.newLine();
         bufferedWriter.flush();
@@ -77,4 +190,39 @@ public class ChattingFormController {
     }
 
 
+    public void sendEmojiOnAction(ActionEvent actionEvent) throws IOException {
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose a Image");
+        File file = fileChooser.showOpenDialog(null);
+
+        bufferedWriter.write("img" + userName + " -" + file.getPath());
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+        hBox.setPadding(new Insets(5, 5, 5, 10));
+
+        Text text = new Text("Me : ");
+        TextFlow textFlow = new TextFlow(text);
+        textFlow.setStyle("-fx-font-weight: bold;"+"-fx-background-color:#cf8bf6;");
+        textFlow.setPadding(new Insets(5, 10, 5, 10));
+        text.setFill(Color.color(1, 1, 1, 1));
+
+
+        ImageView imageView = new ImageView();
+        //Setting image to the image view
+        imageView.setImage(new Image(new File(file.getPath()).toURI().toString()));
+        //Setting the image view parameters
+        imageView.setFitWidth(300);
+        imageView.setPreserveRatio(true);
+
+        hBox.getChildren().add(textFlow);
+        hBox.getChildren().add(imageView);
+
+        vBOxMessage.getChildren().add(hBox);
+
+
+    }
 }
